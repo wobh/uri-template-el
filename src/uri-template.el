f00fa05d9419700dfef5ffb9e9dc3ef553ad5b45
William Clifford <wobh@yahoo.com>
2016-01-08 00:21:16 -0800

    Setup uri-template-el project

---
+++ src/uri-template.el
@@ -0,0 +1,32 @@
+;;; uri-templates.el --- URI templates for Emacs     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016 William Clifford
+
+;; Author: William Clifford <wobh@yahoo.com>
+;; Keywords: comm
+
+;; This file is not part of Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Emacs Lisp implementation of RFC 6570.
+
+;;; Code:
+
+
+
+(provide 'uri-template)
+;;; uri-templates.el ends here
